4ab10496a758b333d6555d6b255d421642d579a3
Henrik Lied <henriklied@Henrik-Lieds-MacBook-Pro.local>
2009-10-29 15:27:58 +0100

    Changed licence formulation and added LICENCE file

--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
 """
 "THE BEER-WARE LICENSE" (Revision 42):
 ---------------------------------------
-<henriklied@gmail.com> wrote this file. As long as you retain this notice you
-can do whatever you want with this stuff. If we meet some day, and you think
-this stuff is worth it, you can buy me a beer in return. Henrik Lied
+<henriklied@gmail.com> wrote this file on behalf of NRK (NORSK RIKSKRINGKASTING AS). 
+As long as you retain this notice you can do whatever you want with this stuff. 
+If we meet some day, and you think this stuff is worth it, you can buy me a beer in return. 
+
+Henrik Lied
 
 """
 from django.conf.urls.defaults import *
